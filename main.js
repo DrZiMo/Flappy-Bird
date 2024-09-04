0e31fca18daefe4e266f843296578bfc4d0c8286
drzimo <zuhaybhamar@gmail.com>
2024-09-04 13:45:13 +0300

    Added The Welcome Page and Start function

---
+++ main.js
@@ -0,0 +1,32 @@
+const gameBoard = document.querySelector('.game-board');
+const welcome = document.querySelector('.welcome'); 
+const pipes = document.querySelector('.pipes');
+const upperPipe = document.querySelector('.upper-pipe');
+const ground = document.querySelector('.ground');
+
+let speed = 5;
+
+window.onload = welcomePage();
+
+function welcomePage() {
+    welcome.style.setProperty('display', 'flex');
+
+    gameBoard.addEventListener('click', startGame);
+    window.addEventListener('keydown', checkKey);
+}
+
+function checkKey(e) {
+    let key = e.keyCode;
+
+    // space = 32
+    if (key == 32) {
+        startGame()
+        window.removeEventListener('keydown', checkKey);
+    }
+}
+
+function startGame() {
+    welcome.style.setProperty('display', 'none');
+    ground.style = `animation: move ${speed}s linear infinite;`;
+    console.log("started the game");
+}
